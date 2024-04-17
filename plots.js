4e65eb2a169595af2aa09d58eaa74ab30a18ecd2
jttran97 <jttran97@gmail.com>
2024-04-16 22:31:28 -0500

    o

--- plots.js
+++ plots.js
@@ -1,54 +1,134 @@
-// Initialized arrays
-let names = []
-let greekNames = []
-let romanNames = []
-let greekSearchResults = []
-let romanSearchResults = []
+const url = "file:///path/total_combined_datat.json"
 
-// For loop to populate arrays
-for (let i = 0; i < searchResults.length; i++) {
-  row = searchResults[i];
-  names.push(row.pair);
-  greekNames.push(row.greekName);
-  romanNames.push(row.romanName);
-  greekSearchResults.push(row.greekSearchResults);
-  romanSearchResults.push(row.romanSearchResults);
+// Read the JSON file from the URL
+d3.json(url).then(function(data) {
+  console.log(data);
+});
+
+
+function init() {
+  // Dropdown Menu
+  let dropdownMenu = d3.select("#selDataset");
+
+  // Variable for sample names
+  let names = data.map(entry => entry.County);
+
+  // Add samples to dropdown menu
+  names.forEach((name) => {
+    dropdownMenu.append("option").text(name).property("county", name);
+  });
+
+  // Set the first sample of the list
+  let samplechart = data[0];
+
+  // Get value of the samples
+  console.log(samplechart);
+
+  // Plots 
+  barchart(samplechart);
+  bubblechart(samplechart);
 }
 
-// Trace1 for the Greek Data
-let trace1 = {
-  x: names,
-  y: greekSearchResults,
-  text: greekNames,
-  name: "Greek",
-  type: "bar"
+// Call the init function
+init();
+
+// Bar chart 
+function barchart (selectedValue) {
+
+  // D3 to retrieve all the data
+  d3.json(url).then((data) => {
+
+      // Retrieve all samples data
+      let samples = data.samples;
+
+      // Call the custom function with filter()
+      let filteredData = samples.filter((sample) => sample.id === selectedValue);
+
+      // First index of the array
+      let obj = filteredData[0];
+
+      // Use otu_ids for the x values, sample_values for the y values, and otu_labels for text values
+      let otu_ids = obj.otu_ids;
+      let otu_labels = obj.otu_labels;
+      let sample_values = obj.sample_values;
+
+      // Log data to console
+      console.log(otu_ids,otu_labels,sample_values);
+
+      // Trace for bar chart
+      let trace = [{
+           x: sample_values.slice(0,10).reverse(),
+           y: otu_ids.slice(0,10).map((otu_id) => `OTU ${otu_id}`).reverse(),
+           text: otu_labels.slice(0,10).reverse(),
+           type: "bar",
+           marker: {
+                color: "#4682B4"
+            },
+            orientation: "h"
+        }];
+       
+        // Use Plotly to plot bar chart
+        Plotly.newPlot("bar", trace);
+    });
+}
+
+
+// Bubble chart
+function bubblechart(selectedValue) {
+
+  // D3 to retrieve all the data
+  d3.json(url).then((data) => {
+      
+      // Retrieve all sample data
+      let samples = data.samples;
+
+      // Call the custom function with filter()
+      let filteredData = samples.filter((sample) => sample.county === selectedValue);
+
+      // First index of the array
+      let obj = filteredData[0];
+
+      // Get the otu_ids, lables, and sample values
+      let otu_ids = obj.otu_ids;
+      let otu_labels = obj.otu_labels;
+      let sample_values = obj.sample_values;
+
+      // Log data to console
+      console.log(otu_ids,otu_labels,sample_values);
+      
+      // Trace for bubble chart
+      let trace1 = {
+          x: otu_ids,
+          y: sample_values,
+          text: otu_labels,
+          mode: "markers",
+          marker: {
+              size: sample_values,
+              color: otu_ids,
+              colorscale: "Earth"
+          }
+      };
+
+      // Apply layout
+      let layout = {
+          xaxis: {title: "OTU ID"}
+      };
+
+      // Use Plotly to plot bubble chart
+      Plotly.newPlot("bubble", [trace1], layout)
+  });
 };
 
-// Trace 2 for the Roman Data
-let trace2 = {
-  x: names,
-  y: romanSearchResults,
-  text: romanNames,
-  name: "Roman",
-  type: "bar"
+// Updates plots when sample is changed
+function optionChanged(selectedValue) { 
+
+  // Log data to new values
+  console.log(selectedValue); 
+
+  // Plot all functions
+  barchart(selectedValue);
+  bubblechart(selectedValue);
+
 };
 
-// Create data array
-let data = [trace1, trace2];
-
-// Apply a title to the layout
-let layout = {
-  title: "Greek vs Roman gods search results",
-  barmode: "group",
-  // Include margins in the layout so the x-tick labels display correctly
-  margin: {
-    l: 50,
-    r: 50,
-    b: 200,
-    t: 50,
-    pad: 4
-  }
-};
-
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+init();
