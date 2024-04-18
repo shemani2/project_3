a8b2f4b7f15c9aac63411e1aa3ed48bf54d973e5
jttran97 <jttran97@gmail.com>
2024-04-17 22:04:13 -0500

    ex

--- plots.js
+++ plots.js
@@ -1,4 +1,4 @@
-const url = "https://raw.githubusercontent.com/shemani2/project_3/jenni/total_combined_datat.json"
+const url = "https://raw.githubusercontent.com/shemani2/project_3/jenni/total_combined_data.json"
 
 // Read the JSON file from the URL
 d3.json(url).then(function(data) {
